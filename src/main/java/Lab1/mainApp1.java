186e9e0e958226624831a4993c2fd643bbe6b016
Li Hung Kit <lhjzjkzard@gmail.com>
2026-09-20 18:22:59 +0800

    Add 2 print lines as report heading

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -2,6 +2,8 @@ package Lab1;
 
 public class mainApp1 {
     public static void main(String[] args) {
+        System.out.println("==== Comp3111LEx Lab Report ====");
+        System.out.println("---- power & factorial demo ----");
         System.out.println("power(2, 10) = " + myLibrary.power(2, 10));
         System.out.println("factorial(5) = " + myLibrary.factorial(5));
     }
