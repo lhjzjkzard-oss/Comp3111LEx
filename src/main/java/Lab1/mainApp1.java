bbb27ad822b2328b963c6cb30294973b0797e675
Li Hung Kit <lhjzjkzard@gmail.com>
2026-09-20 18:22:51 +0800

    This is an initial commit for Comp3111LEx Project

---
+++ src/main/java/Lab1/mainApp1.java
@@ -0,0 +1,8 @@
+package Lab1;
+
+public class mainApp1 {
+    public static void main(String[] args) {
+        System.out.println("power(2, 10) = " + myLibrary.power(2, 10));
+        System.out.println("factorial(5) = " + myLibrary.factorial(5));
+    }
+}
---
+++ src/main/java/Lab1/myLibrary.java
@@ -0,0 +1,21 @@
+package Lab1;
+
+public class myLibrary {
+    // returns base raised to the power of exponent (exponent >= 0)
+    public static long power(int base, int exponent) {
+        long result = 1;
+        for (int i = 0; i < exponent; i++) {
+            result *= base;
+        }
+        return result;
+    }
+
+    // returns the factorial of n (n >= 0)
+    public static long factorial(int n) {
+        long result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
